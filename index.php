c3d88ab0319c41629e41440159dabe64535a647f
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2020-05-08 01:23:06 -0300

    Connect to DB using env vars and output something

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-phpinfo();
+require_once('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$connection = getenv('CONNECTION');
+$database = getenv('DATABASE');
+$connectionString = "mysql:host={$connection};dbname={$database}";
+$username = getenv('USERNAME');
+$password = getenv('PASSWORD');
+
+$pdo = new PDO($connectionString, $username, $password); 
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES,false);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+foreach($pdo->query('SELECT * FROM my_test_table') as $row) {
+    print_r($row);
+}
